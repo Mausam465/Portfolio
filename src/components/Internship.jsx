d071a19f2b15273da456e1c0cc6b0b2c7959cb63
Mausam Kumari <mausamkumari9095@gmail.com>
2026-03-23 01:47:03 +0530

    update

--- src/components/Internship.jsx
+++ src/components/Internship.jsx
@@ -15,7 +15,7 @@ const internships = [
     ],
   },
   {
-    role: 'Web Design Intern',
+    role: 'Software Developer Intern',
     company: 'AT Production Pvt. Ltd.',
     link: 'https://www.atproduction.net/',
     duration: 'Oct 2025 - Feb 2026',
